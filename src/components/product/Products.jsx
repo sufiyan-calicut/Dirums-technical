09a5a2d3eef8ad7f6d51cfa728323fa05a9fefb1
sufiyan-calicut <sufiyanemcalicut@gmail.com>
2024-02-17 09:23:47 +0530

    updation of product view

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -70,21 +70,23 @@ const Products = () => {
         </button>
       </div>
       <div
-        className={`ProductsContainer w-full h-auto mt-2 sm:mt-0 relative grid  ${!isSingleView && 'grid-cols-2'} md:grid-cols-3 sm:gap-2`}
+        className={`ProductsContainer w-full h-auto mt-2 sm:mt-0 relative grid  ${
+          !isSingleView && 'grid-cols-2'
+        } md:grid-cols-3 sm:gap-2`}
       >
         {productsData.map(product => (
           <div
             key={product.id}
-            className={`max-w-fit min-h-fit mt-4 sm:mt-4 ${
-              isSingleView ? 'w-full' : 'w-11/12 md:w-1/3'
-            } transition-all duration-700 left-0 right-0 mx-auto text-wrap`}
+            className={`max-w-fit min-h-fit mt-4 ${
+              isSingleView ? 'w-full m-2' : 'w-11/12 md:w-1/3'
+            } transition-all duration-700 left-0 right-0  text-wrap`}
           >
             <div
               className={` ${
-                isSingleView ? 'h-60' : 'h-40'
-              }    max-h-70 max-w-3/4 relative`}
+                isSingleView ? 'h-fit' : 'h-44'
+              }    max-h-70  relative`}
             >
-              <div className="w-8 h-8 bg-gradient-to-b from-white to-gray-300 rounded-full absolute right-2 top-2  shadow-2xl flex items-center justify-center">
+              <div className="w-9 h-9 bg-gradient-to-b from-white to-gray-300 rounded-full absolute right-3 top-3 shadow-lg flex items-center justify-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -95,17 +97,18 @@ const Products = () => {
                   <path
                     d="M12.0054 21.7803L12.0047 21.7797C8.4831 18.8307 5.60033 16.4128 3.59235 14.1442C1.58811 11.8797 0.516698 9.83022 0.516698 7.627C0.516698 4.03641 3.56471 1.18039 7.53715 1.18039C9.77854 1.18039 11.9279 2.14638 13.3223 3.64668L13.6739 4.02501L14.0255 3.64668C15.4199 2.14638 17.5693 1.18039 19.8106 1.18039C23.7831 1.18039 26.8311 4.03641 26.8311 7.627C26.8311 9.83022 25.7597 11.8797 23.7555 14.1442C21.7475 16.4128 18.8647 18.8307 15.3431 21.7797L15.3424 21.7803L13.6739 23.183L12.0054 21.7803Z"
                     stroke="black"
-                    strokeWidth="0.960033"
+                    strokeWidth="1"
                   />
                 </svg>
               </div>
+
               <img
                 src={product.image}
                 alt="product img"
                 className="rounded-md w-full h-full object-cover "
               />
             </div>
-            <div className="">
+            <div className="py-2">
               <h3
                 className={`text-darkFontOrange font-bold ${
                   isSingleView ? 'text-base' : 'text-xs'
@@ -113,20 +116,22 @@ const Products = () => {
               >
                 {product.productName}
               </h3>
-              <p
-                className={`text-themeBlack ${
-                  isSingleView ? 'text-base' : 'text-xs'
-                }`}
-              >
-                {product.artistName}
-              </p>
-              <p
-                className={`text-themeBlack ${
-                  isSingleView ? 'text-base' : 'text-xs'
-                }`}
-              >
-                {product.category}
-              </p>
+              <div className="py-2">
+                <p
+                  className={`text-themeBlack ${
+                    isSingleView ? 'text-sm' : 'text-xs'
+                  }`}
+                >
+                  {product.artistName}
+                </p>
+                <p
+                  className={`text-themeBlack ${
+                    isSingleView ? 'text-sm' : 'text-xs'
+                  }`}
+                >
+                  {product.category}
+                </p>
+              </div>
               <div className={`flex w-full justify-between flex-col `}>
                 <div className="priceArea flex gap-2 items-end w-full overflow-clip">
                   <p
@@ -145,7 +150,9 @@ const Products = () => {
                     /-
                   </p>
                 </div>
-                <div className={`buttonArea bg-AddToCart py-2 w-fit rounded-md`}>
+                <div
+                  className={`buttonArea bg-AddToCart mt-2  py-2 w-fit rounded-md`}
+                >
                   <button
                     className={`  flex justify-center gap-1  items-center rounded-md w-36 font-bold overflow-clip  text-sm h-6 ${
                       !isSingleView && 'w-20 h-4 '
@@ -157,7 +164,6 @@ const Products = () => {
                     </div>
                   </button>
                 </div>
-                
               </div>
               {/* <div className="buttonArea bg-AddToCart py-2 w-fit rounded-md">
                   <button
